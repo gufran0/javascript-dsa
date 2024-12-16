bc98a267aa589704b7a088331b035cb34d773d85
gufran0 <md.gufran@techchefz.com>
2024-12-17 00:59:54 +0530

    truthy and falsy values

---
+++ truthyAndFalsyValue.js
@@ -0,0 +1,20 @@
+Boolean(0);
+Boolean(NaN);
+// false
+// in numbers only 0 and nan is false;
+
+Boolean("");
+// empty string is false
+// empty string is not false
+
+Boolean(undefined);
+// false
+Boolean(true);
+// true
+
+// 0 | nan | null | undefined
+
+!0 
+// changes the value
+!!0 
+// chnages it back to what it is
